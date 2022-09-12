1b470239734c19ef9b3501bea722ccc1ef672bd8
maikvas666@gmail.com <maikvas666@gmail.com>
2022-09-11 19:34:01 -0500

    test: fetch good making

--- netlify/functions/count.js
+++ netlify/functions/count.js
@@ -1,6 +1,16 @@
-exports.handler = function (event, context) {
-    return {
-      statusCode: 202,
-      body: JSON.stringify({ message: "Hello World" }),
-    };
+import fetch from "node-fetch";
+
+const info = fetch("https://api.chucknorris.io/jokes/random")
+.then(async response => response.json())
+.then(data => {
+  return data
+});
+
+exports.handler = async function (event, context) {
+  const data =  { message:await info }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
   };
+};
